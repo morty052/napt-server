b0b491ab3333548604aea4803cd0457eda6afbd1
Morty052 <anomaly619052@gmail.com>
2023-11-12 14:25:49 -0800

    napt-server

---
+++ server.ts
@@ -0,0 +1,20 @@
+import express from "npm:express";
+import { createServer } from "node:http";
+import { Server } from "npm:socket.io";
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  /* options */
+});
+
+app.get("/", (req, res) => {
+  console.log("request received");
+  res.send("Hello World!");
+});
+
+io.on("connection", (socket) => {
+  // ...
+});
+
+httpServer.listen(3000);
