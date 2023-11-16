0ae95923ed08599712695c11ccd3d9b6751e83ff
Morty052 <anomaly619052@gmail.com>
2023-11-16 07:44:48 -0800

    attempting to store user

--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -6,4 +6,13 @@ userRoutes.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+userRoutes.get("/createuser", (req, res) => {
+  const { email, password } = req.query;
+  res.send({
+    id: "abyhddkg67",
+    email,
+    password,
+  });
+});
+
 export default userRoutes;
