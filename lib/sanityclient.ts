88357991f261a399e70e91671f83ce91b58ba51b
Morty052 <anomaly619052@gmail.com>
2023-11-27 00:15:55 -0800

    changed sanity to npm

--- lib/sanityclient.ts
+++ lib/sanityclient.ts
@@ -1,4 +1,4 @@
-import { createClient } from "https://esm.sh/@sanity/client@6.8.6";
+import { createClient } from "npm:@sanity/client@6.8.6";
 
 export const client = createClient({
   projectId: "hxg3wrvl",
